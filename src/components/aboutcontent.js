363fe6c6d4237c212d6613133033c09094499a4f
Dwayne Levene <39275855+dwayneglevene@users.noreply.github.com>
2023-05-09 22:34:38 -0700

    added a new lesson

--- src/components/aboutcontent.js
+++ src/components/aboutcontent.js
@@ -5,7 +5,7 @@ function About(){
             
             In the past year I have taught Snap, P5, JavaScript,Web design, Mongo DB, and Flask and the list is growing.That is the thing about tech; as you do, you learn ! 
             
-            <h5>CS ed thoughts:</h5>I look at everyday as a learning experience and have embedded my self in non stop learning ever since I started on this coding journey.Although I like teaching I am still open to new opportunities to develop. My goal is to be a full fledged engineer as well as start my own tech company.My hopes on creating this portfolio are that I meet and connect with other like minded individuals and work towards those goals.I would love to talk more.
+            <h5>CS ed thoughts:</h5>I look at everyday as a learning experience and have embedded my self in non stop learning ever since I started on this coding journey.Although I like teaching I am still open to new opportunities to develop. My goal is to be a full fledged engineer as well as start my own tech company.I think its great that we are empowering young minds with the knowledge to do the same.
        
         <button onClick ={() => window.open('https://docs.google.com/document/d/15sKeNwcIksUeDkZu8AJglUCd_Hh9o_FTaePV587PNs8/edit?usp=sharing')} type="button"  class="btn btn-primary newButton">Resume</button>
         </div>
