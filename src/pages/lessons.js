03910015ba6af4de51e61e6d753105fd437a5370
Dwayne Levene <39275855+dwayneglevene@users.noreply.github.com>
2023-05-09 22:14:07 -0700

    new update

--- src/pages/lessons.js
+++ src/pages/lessons.js
@@ -26,11 +26,51 @@ function Lessons(){
                     <div class="row slides">
                     <div class ="col">
                     <h2>Student example</h2>
-                      <h3> <a href="https://codehs.com/share/id/project-for-competition-adDKlI/run">Student Work example</a></h3> 
-                      <h3> <a href="https://codehs.com/share/id/practice-EpTL4p/run">Student example 2</a></h3> 
-                      https://codehs.com/share/id/practice-o6tOHP/run
-
+                      <h3> <a href="https://codehs.com/share/id/project-for-competition-adDKlI/run">Flower petals</a></h3> 
+                      <h3> <a href="https://codehs.com/share/id/practice-EpTL4p/run">Speedracer</a></h3> 
                       <h3> <a href=" https://codehs.com/share/id/practice-o6tOHP/run">Beyblade</a></h3> 
+
+                    </div>
+
+
+                    </div>
+
+
+
+               
+               
+            </div>
+
+
+
+
+
+            {/* Next set */}
+
+            <div class="container">
+                <div class="row slides">
+                    <div class ="col">
+                        <h1>Python</h1>
+                        <h2>Presentation</h2>
+                        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQBShqWMjDgaIhwMv-yW2BxDAgrTFslMvdIGMhnhuLU8PRgT9MaS3kYYZBtUDRrKIvtw4a9mAS4Zn65/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    </div>
+                    </div>
+
+                    <div class="row slides">
+                    <div class ="col">
+                       <h2>Lesson plan</h2>
+                        <iframe  class="lp" src="https://docs.google.com/document/d/e/2PACX-1vR_XHYXwBekGghJWShcve4i-xkbsgEF6GKK881jJXrXCGlwJbjjuVSr46waS4YgiFCc08rQwuYeJc0p/pub?embedded=true"></iframe>
+                        
+                    </div>
+
+
+                    </div>
+
+                    <div class="row slides">
+                    <div class ="col">
+                    <h2>Student example</h2>
+                      <h3> <a href="https://codehs.com/share/id/beaded-bracelet-XPd9VL/run">Jamaica Inspired Jewelry</a></h3> 
+                      <h3> <a href="https://codehs.com/share/id/beaded-bracelet-wbdSw6/run">Valentine's day gift</a></h3> 
 
                     </div>
 
